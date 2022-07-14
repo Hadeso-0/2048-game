dd4a4e43dca0ed39830766fba704aa74a79f8706
Manav Agarwal <manavagarwal1410@gmail.com>
2022-07-14 19:34:42 +0530

    tile styles

--- scripts/tile.js
+++ scripts/tile.js
@@ -1,3 +1,17 @@
+const tileStyle = [
+  { background: '#eee4da', color: '#776e65', key: 2 },
+  { background: '#ede0c8', color: '#776e65', key: 4 },
+  { background: '#f2b179', color: '#f9f6f2', key: 8 },
+  { background: '#f59563', color: '#f9f6f2', key: 16 },
+  { background: '#f67c5f', color: '#f9f6f2', key: 32 },
+  { background: '#f65e3b', color: '#f9f6f2', key: 64 },
+  { background: '#edcf72', color: '#f9f6f2', key: 128 },
+  { background: '#edcc61', color: '#f9f6f2', key: 256 },
+  { background: 'tedc850', color: '#f9f6f2', key: 512 },
+  { background: '#edc53f', color: '#f9f6f2', key: 1024 },
+  { background: '#edc22e', color: '#f9f6f2', key: 2048 },
+]
+
 export default class Tile {
   // private vars
   #tileElement
@@ -19,15 +33,11 @@ export default class Tile {
     this.#value = v
     this.#tileElement.textContent = v
     const pow = Math.log2(v)
-    const backgroundLightness = 100 - pow * 9
     this.#tileElement.style.setProperty(
-      '--background-lightness',
-      `${backgroundLightness}%`,
+      '--background',
+      `${tileStyle[pow - 1].background}`,
     )
-    this.#tileElement.style.setProperty(
-      '--text-lightness',
-      `${backgroundLightness <= 50 ? 90 : 10}%`,
-    )
+    this.#tileElement.style.setProperty('--text', `${tileStyle[pow - 1].color}`)
   }
 
   set x(value) {
